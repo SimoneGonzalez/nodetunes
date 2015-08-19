0205daecd5a44c954df85beb6ba3e35ce43b00b1
Simone Gonzalez <simone.b.gonzalez@gmail.com>
2015-08-19 11:59:14 -0500

    Added requirements

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var artist = require('./routes/artist');
+var artists = require('./routes/artists');
+
+
+var app = express();
+
+require('./lib/secrets');
+require('./lib/mongodb');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', routes);
+app.use('/artist', artist);
+app.use('/artists', artists);
+app.use(express.static('www'));
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
